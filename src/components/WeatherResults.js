c76ae523e143f76578dd7d3e2707641a521cf7bc
Vitamin <vitamin@ukr.net>
2021-05-05 23:14:04 +0300

    add useState & useEffect

--- src/components/WeatherResults.js
+++ src/components/WeatherResults.js
@@ -1,65 +1,51 @@
 /** @jsx createElement */
 /** @jsxFrag createFragment */
-import { createElement, createFragment } from '../framework/element';
-import renderApp from '../framework/render';
+import { createElement, createFragment, useState } from '../framework';
+import { CELSIUS_UNITS } from '../utils';
 
 import UnitSwitch from './UnitSwitch';
 import WeatherToday from './WeatherToday';
 import WeatherForecast from './WeatherForecast';
-import {
-  getCurrentCityWeatherData,
-  getFilteredByDateWeatherData,
-  isCurrentCityDataLoaded,
-} from '../data/weatherData';
+import { getFilteredByDateWeatherData } from '../data/weatherData';
 
-function setCurrentUnits(value) {
-  window.dataStore.currentUnits = value;
-  renderApp();
-}
+function WeatherResults({ isLoading, error, currentCity, weatherData: cityByWeather }) {
+  const [currentUnits, setCurrentUnits] = useState(CELSIUS_UNITS);
 
-function WeatherResults() {
-  const { isDataLoading, currentUnits, error, currentCity } = window.dataStore;
-  let content = null;
-
-  if (currentCity === '') {
-    content = 'Search by city name';
-  } else {
-    if (error !== null) {
-      content = error;
-    }
-
-    if (isDataLoading) {
-      content = 'Loading...';
-    }
-
-    if (isCurrentCityDataLoaded()) {
-      const weatherData = getCurrentCityWeatherData();
-      const weatherTodayData = getFilteredByDateWeatherData(weatherData, { includeBaseDate: true });
-      const weatherForecastData = getFilteredByDateWeatherData(weatherData, {
-        includeDatesAfterBase: true,
-      });
-      content = (
-        <>
-          <UnitSwitch currentUnits={currentUnits} setCurrentUnitsCB={setCurrentUnits} />
-          <br />
-          <WeatherToday
-            currentUnits={currentUnits}
-            currentCity={currentCity}
-            weatherData={weatherTodayData}
-          />
-          <br />
-          <br />
-          <WeatherForecast
-            currentUnits={currentUnits}
-            currentCity={currentCity}
-            weatherData={weatherForecastData}
-          />
-        </>
-      );
-    }
+  if (!currentCity) {
+    return <div>Search by city name</div>;
   }
 
-  return <p>{content}</p>;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  const weatherData = cityByWeather?.list;
+  const weatherTodayData = getFilteredByDateWeatherData(weatherData, { includeBaseDate: true });
+  const weatherForecastData = getFilteredByDateWeatherData(weatherData, {
+    includeDatesAfterBase: true,
+  });
+  return (
+    <>
+      <UnitSwitch currentUnits={currentUnits} setCurrentUnits={setCurrentUnits} />
+      <br />
+      <WeatherToday
+        currentUnits={currentUnits}
+        currentCity={currentCity}
+        weatherData={weatherTodayData}
+      />
+      <br />
+      <br />
+      <WeatherForecast
+        currentUnits={currentUnits}
+        currentCity={currentCity}
+        weatherData={weatherForecastData}
+      />
+    </>
+  );
 }
 
 export default WeatherResults;
