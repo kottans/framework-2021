a59b906b48b4ed92896403bd625df224c5ad9c0e
OleksiyRudenko <oleksiy.rudenko++git@gmail.com>
2021-05-18 20:32:03 +0200

    app(Checkbox): replace style class string definition with a list of classes

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -5,7 +5,7 @@ import styles from './Checkbox.css';
 
 export default function Checkbox({ label = '', onChange = null }) {
   return (
-    <label className={`${styles.check} ${styles.option}`}>
+    <label className={[styles.check, styles.option]}>
       <input className={styles.check__input} type="checkbox" onChange={onChange} />
       <span className={styles.check__box}></span>
       {label}
