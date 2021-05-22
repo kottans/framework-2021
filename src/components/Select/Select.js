5735c2be521a0cca5996764ed6e4568873ff6fbd
f3d0t <logrus7@gmail.com>
2021-05-23 02:27:48 +0300

    Select component: merge components into one file

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -2,8 +2,6 @@
 /** @jsxFrag createFragment */
 
 import { createElement, createFragment } from '../../framework/element';
-import Option from './Option';
-import Optgroup from './Optgroup';
 
 /**
  * Creates select element.
@@ -67,7 +65,7 @@ export default function Select({
   });
   return (
     <>
-      {label ? <label for={id}>{label}:</label> : ''}
+      {label ? <label for={id}>{label}: </label> : ''}
       <select
         id={id}
         multiple={isMultiple}
@@ -80,5 +78,44 @@ export default function Select({
         {options}
       </select>
     </>
+  );
+}
+
+/**
+ * Creates option element.
+ * @param {string} value - string; used as text if label property is not defined or falsy, or as optgroup label if object has options property defined
+ * @param {string} label - optional, string; if not defined or null or undefined then value is used as an option UI representation visible to a user;
+ * @param {boolean} disabled: optional, boolean; if defined then current object will be disabled
+ * @param {boolean} selected: optional, boolean; if defined then current object will be selected
+ */
+
+export function Option({ value, label = null, disabled = null, selected = null }) {
+  return (
+    <option value={value} selected={selected} disabled={disabled}>
+      {label ? label : value}
+    </option>
+  );
+}
+
+/**
+ * Creates optgroup element.
+ * @param {string} label - string; value is used as an option UI representation visible to a user;
+ * @param {boolean} disabled: optional, boolean; if defined then current object will be disabled
+ * @param {boolean} selectedOption: optional, boolean; if defined then current object will be selected
+ * @param {array} options: array of option objects or array of values, index is used as an option id (option value attribute) and array element itself as an option UI representation visible to a user
+ */
+
+export function Optgroup({ label, disabled = null, selectedOption = null, options }) {
+  return (
+    <optgroup label={label} disabled={disabled}>
+      {options.map(groupOption => (
+        <Option
+          value={groupOption.value}
+          label={groupOption.label ? groupOption.label : groupOption.value}
+          selected={groupOption.value === selectedOption}
+          disabled={groupOption.disabled}
+        />
+      ))}
+    </optgroup>
   );
 }
